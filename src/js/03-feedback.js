21fc557505bd9bb8865580fd4b1f2412cc53652b
FrYt4 <adrian.fryczkie@yahoo.com>
2024-02-07 18:45:00 +0100

    hwThird

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector("input[name='email']");
+const message = form.querySelector("textarea[name='message']");
+
+const storageKey = "feedback-form-state";
+
+const saveToStorage = throttle(() => {
+    const state = {
+        email:email.value.trim(),
+        message:message.value.trim(),
+    };
+    localStorage.setItem(storageKey, JSON.stringify(state));
+},500);
+
+form.addEventListener(`input`, saveToStorage);
+
+const loadToStorage = () => {
+    const stored = localStorage.getItem(storageKey);
+    if(stored){
+        try {
+            const state = JSON.parse(stored);
+            email.value = state.email;
+            message.value = state.message;
+        } catch(error) {
+            console.log(error.message);
+        } 
+    }
+};
+
+loadFromStorage();
+
+
+form.addEventListener(`submit` , event => {
+    event.preventDefault();
+    if(email.value === `` || message.value === ``){
+        alert(`Please fill all the fields`);
+    } else{
+        console.log(`Form Data:` ,{
+            email:email.value,
+            message: message.value,
+        });
+    }
+})
