dea9f99f61ed8d3cab094adb18bdf4c01c501687
FrYt4 <adrian.fryczkie@yahoo.com>
2024-02-07 17:47:58 +0100

    firstHw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const galleryArray = galleryItems.map(
+  item =>
+    `<li><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a></li>`
+);
+
+gallery.innerHTML = galleryArray.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
